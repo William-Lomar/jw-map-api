8f6fa36249a5daf2914e595ae7318cef60caeb25
William Lomar <lomarw99@gmail.com>
2024-07-20 12:45:16 -0300

    init nest

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(Number(process.env.HTTP_PORT));
+}
+bootstrap();
